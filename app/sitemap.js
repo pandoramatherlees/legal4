3d5ce0e50be3ba5c00aa78b8935237d1acaa6a12
pandoramatherlees <pandora.matherlees@gmail.com>
2026-03-17 13:19:29 +0000

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -11,5 +11,6 @@ export default function sitemap() {
     { url: `${baseUrl}/news-articles/uk-global-talent-visa-guide`, lastModified: new Date() },
     { url: `${baseUrl}/news-articles/global-talent-visa-guide-2026`, lastModified: new Date() },
     { url: `${baseUrl}/news-articles/uk-evisa-digital-immigration-system`, lastModified: new Date() },
+    { url: `${baseUrl}/news-articles/new-sponsor-compliance-duties-2026`, lastModified: new Date() },
   ];
 }
