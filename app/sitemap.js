b8cf7af2cd50a211a2d125494e6fe26e5603d81d
pandoramatherlees <pandora.matherlees@gmail.com>
2026-04-13 11:12:06 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -14,5 +14,6 @@ export default function sitemap() {
     { url: `${baseUrl}/news-articles/new-sponsor-compliance-duties-2026`, lastModified: new Date() },
     { url: `${baseUrl}/news-articles/uk-immigration-settlement-reforms-2026`, lastModified: new Date() },
 { url: `${baseUrl}/news-articles/uk-eu-youth-mobility-scheme-emergency-brake`, lastModified: new Date() },
-  { url: `${baseUrl}/news-articles/uk-global-talent-visa-artists`, lastModified: new Date() },];
+  { url: `${baseUrl}/news-articles/uk-global-talent-visa-artists`, lastModified: new Date() },
+    { url: `${baseUrl}/news-articles/end-of-pre-settled-status-home-office-enforcement`, lastModified: new Date() },];
 }
