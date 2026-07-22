b98851b42cd51ee81eecb76ae9eb4f8dde6d3e71
pandoramatherlees <pandora.matherlees@gmail.com>
2026-07-22 16:41:22 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -31,5 +31,6 @@ export default function sitemap() {
   { url: `${baseUrl}/news-articles/lovers-and-lovebirds-financial-criteria`, lastModified: new Date() },
     { url: `${baseUrl}/news-articles/lovers-and-lovebirds-professional-help`, lastModified: new Date() },
     { url: `${baseUrl}/news-articles/spouse-visa-common-reasons-for-refusal`, lastModified: new Date() },
+    { url: `${baseUrl}/news-articles/lovers-and-lovebirds-spouse-visa-requirements`, lastModified: new Date() },
 ];
 }
