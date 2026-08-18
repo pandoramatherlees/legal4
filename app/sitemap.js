5df53d756531253ec45c3d339e23459353095d5a
pandoramatherlees <pandora.matherlees@gmail.com>
2026-08-18 18:31:04 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -38,5 +38,6 @@ export default function sitemap() {
     { url: `${baseUrl}/news-articles/the-skill-tree-obtaining-indefinite-leave-to-remain-ilr`, lastModified: new Date() },
     { url: `${baseUrl}/news-articles/skilled-worker-visa-successful-application`, lastModified: new Date() },
     { url: `${baseUrl}/news-articles/global-talent-visa-expands-researchers`, lastModified: new Date() },
+    { url: `${baseUrl}/news-articles/sponsored-and-silenced-health-care-worker-visa`, lastModified: new Date() },
   ];
 }
